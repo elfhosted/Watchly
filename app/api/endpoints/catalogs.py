66d2112c5345d64051bf160fea9fcaccd90c0885
Bimal Timilsina <timilsina.bml05@gmail.com>
2025-11-16 15:11:21 +0545

    feat: implement manual catalog update endpoint and streamline catalog retrieval
    - Replaced the PATCH method with a GET method for updating catalogs.
    - Enhanced the catalog update logic to fetch and process loved and watched items from the library.
    - Improved error handling and logging for catalog updates.
    - Updated the streams endpoint to include a link for manual catalog updates.

--- app/api/endpoints/catalogs.py
+++ app/api/endpoints/catalogs.py
@@ -68,22 +68,65 @@ async def get_catalog(
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@router.patch("/")
-async def update_catalogs(response: Response):
+@router.get("/update")
+async def update_catalogs():
     """
-    Update the catalogs for the addon.
+    Update the catalogs for the addon. This is a manual endpoint to update the catalogs.
     """
-    # steps
-    # 1. Log in to stremio
-    # 2. Fetch addons and find this addon
-    # 3. Update catalogs
-    # 4. Update manifest in addons in stremio
-    try:
-        stremio_service = StremioService()
-        auth_key = await stremio_service._get_auth_token()
-        # find catalogs to update
-        catalogs = []
-        stremio_service.update_catalogs(catalogs, auth_key)
-    except Exception as e:
-        logger.error(f"Error updating catalogs: {e}", exc_info=True)
-        raise HTTPException(status_code=500, detail=str(e))
+    stremio_service = StremioService()
+    library_items = await stremio_service.get_library_items()
+    seen_items = set()
+    catalogs = []
+    seed = {
+        "watched": {
+            "movie": False,
+            "series": False,
+        },
+        "loved": {
+            "movie": False,
+            "series": False,
+        },
+    }
+    # checked loved items
+    loved_items = library_items.get("loved", [])
+    watched_items = library_items.get("watched", [])
+    # now find first few items
+    for l_item in loved_items:
+        print(l_item)
+        type_ = l_item.get("type")
+        if type_ in ["tv"]:
+            type_ = "series"
+        if l_item.get("_id") in seen_items or seed["loved"][type_]:
+            continue
+        seen_items.add(l_item.get("_id"))
+        seed["loved"][type_] = True
+        catalogs.append(
+            {
+                "type": type_,
+                "id": l_item.get("_id"),
+                "name": f"Because you Loved {l_item.get('name')}",
+                "extra": [],
+            }
+        )
+    for w_item in watched_items:
+        type_ = l_item.get("type")
+        if type_ in ["tv"]:
+            type_ = "series"
+
+        if w_item.get("_id") in seen_items or seed["watched"][type_]:
+            continue
+        seen_items.add(w_item.get("_id"))
+        seed["watched"][type_] = True
+        catalogs.append(
+            {
+                "type": type_,
+                "id": w_item.get("_id"),
+                "name": f"Because you Watched {w_item.get('name')}",
+                "extra": [],
+            }
+        )
+    # update catalogs
+    auth_key = await stremio_service._get_auth_token()
+    updated = await stremio_service.update_catalogs(catalogs, auth_key)
+    logger.info(f"Updated catalogs: {updated}")
+    return {"success": updated}
