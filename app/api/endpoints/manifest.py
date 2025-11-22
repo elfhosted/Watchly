b6e9f52b4af7b34e0f43599952dd2e2e5f013b95
Bimal Timilsina <timilsina.bml05@gmail.com>
2025-11-22 14:55:42 +0545

    feat: refresh catalogs when installing

--- app/api/endpoints/manifest.py
+++ app/api/endpoints/manifest.py
@@ -1,20 +1,19 @@
 from fastapi.routing import APIRouter
 
 from app.core.config import settings
+from app.utils import resolve_user_credentials
+from app.services.stremio_service import StremioService
+from app.services.catalog import DynamicCatalogService
+
 
 router = APIRouter()
 
 
-@router.get("/manifest.json")
-@router.get("/{token}/manifest.json")
-async def manifest(token: str | None = None):
-    """Stremio manifest endpoint with optional credential token in the path."""
-    # Cache manifest for 1 day (86400 seconds)
-    # response.headers["Cache-Control"] = "public, max-age=86400"
+def get_base_manifest():
     return {
         "id": settings.ADDON_ID,
-        "version": "0.1.0",
-    "name": settings.ADDON_NAME,
+        "version": "0.1.1",
+        "name": settings.ADDON_NAME,
         "description": "Movie and series recommendations based on your Stremio library",
         "logo": "https://raw.githubusercontent.com/TimilsinaBimal/Watchly/refs/heads/main/static/logo.png",
         "resources": [
@@ -29,3 +28,36 @@ async def manifest(token: str | None = None):
         ],
         "behaviorHints": {"configurable": True, "configurationRequired": False},
     }
+
+async def fetch_catalogs(token: str | None = None):
+    if not token:
+        return []
+    credentials = await resolve_user_credentials(token)
+    stremio_service = StremioService(
+        username=credentials.get("username") or "",
+        password=credentials.get("password") or "",
+        auth_key=credentials.get("authKey"),
+    )
+    library_items = await stremio_service.get_library_items()
+    dynamic_catalog_service = DynamicCatalogService(stremio_service=stremio_service)
+    catalogs = await dynamic_catalog_service.get_watched_loved_catalogs(
+        library_items=library_items
+    )
+    catalogs += await dynamic_catalog_service.get_genre_based_catalogs(
+        library_items=library_items
+    )
+    return catalogs
+
+@router.get("/manifest.json")
+@router.get("/{token}/manifest.json")
+async def manifest(token: str | None = None):
+    """Stremio manifest endpoint with optional credential token in the path."""
+    # Cache manifest for 1 day (86400 seconds)
+    # response.headers["Cache-Control"] = "public, max-age=86400"
+
+    base_manifest = get_base_manifest()
+    if token:
+        catalogs = await fetch_catalogs(token)
+        if catalogs:
+            base_manifest["catalogs"] += catalogs
+    return base_manifest
